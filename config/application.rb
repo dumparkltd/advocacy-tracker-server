20af43c490a134ce197eda86246dd22fddefa0cb
tmfrnz <timo@dumpark.com>
2026-01-03 16:37:52 +0100

    Fix CORS - remove credentials with wildcard origins

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,16 @@ module HumanRightsNationalReporting
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: :any,
+          expose: ["access-token", "expiry", "token-type", "uid", "client"]
+      end
+    end
   end
 end
