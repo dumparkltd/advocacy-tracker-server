85745413bfc37c1318acf7a8316fb9bbdd9b33fc
tmfrnz <timo@dumpark.com>
2026-01-06 18:31:52 +0100

    remove obsolete layout and rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::API
   include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  layout :layout_by_resource
-
   before_action :authenticate_user!, only: [:create, :update, :destroy], unless: :devise_controller?
   before_action :authorize!
   after_action :verify_authorized, except: [:index], unless: :devise_controller?
@@ -31,10 +29,6 @@ class ApplicationController < ActionController::API
     serializer.new(target).serializable_hash.to_json
   end
 
-  def layout_by_resource
-    devise_controller? ? "authentication" : "application"
-  end
-
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: {error: e.message}, status: :not_found
   end
@@ -55,11 +49,6 @@ class ApplicationController < ActionController::API
   private
 
   def user_not_authorized
-    if request.format == "application/json"
-      return render json: {error: "not authorized"}, status: 403
-    end
-
-    flash[:error] = "You are not authorized to perform this action."
-    redirect_to(root_path)
+    render json: {error: "not authorized"}, status: :forbidden
   end
 end
